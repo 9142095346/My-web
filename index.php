b273724fd6ec2f07f28252222b23f2dc05e39db7
9142095346 <129297323+9142095346@users.noreply.github.com>
2023-03-29 19:21:59 +0530

    added index.html

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>admin</title>
+</head>
+	<body bgcolor="grey">
+		<center><h1><u> <font color="lightpink">Compucom Groups of College </font></u></h1></center>
+		<hr>
+		<marquee><h3>This is marks upload site</h3></marquee>
+		
+		<br><br><br><br><br><br><br><center>
+			<table border="2">
+
+		<tr> 
+			<td>Access to the portal</td>
+		</tr>
+
+		<tr>
+			<td>
+			<a href="stu_reslut.php">Faculty</a>
+			</td>
+		</tr>
+
+		<tr>
+			<td>
+			<a href="student.php">Student</a>
+			</td>
+		</tr>
+
+		<tr>
+				<td> <a href="index.php">Home </a> </td>
+		</tr>
+		
+		</table> </center> 
+	   
+</body>
+</html>
